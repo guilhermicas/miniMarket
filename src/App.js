029f09e141a9e29c21c9435645f3ac1bda6feba9
guilhermicas <sotrabalhosgui@hotmail.com>
2021-06-07 22:22:07 +0100

    carrinho e navegaçao

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { commerce } from './lib/commerce'
-import { Products, Navbar } from './components'
+import { Products, Navbar, Cart } from './components'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
     const [products, setProducts] = useState([])
@@ -21,7 +22,7 @@ const App = () => {
 
         setCart(item.cart)
     }
-    
+
     useEffect(() => {
         fetchProducts();
         fetchCart();
@@ -30,10 +31,19 @@ const App = () => {
     console.log(cart)
 
     return (
-        <>
-            <Navbar totalItems={cart.total_items}/>
-            <Products products={products} onAddToCart={handleAddToCart}/>
-        </>
+        <Router>
+            <>
+                <Navbar totalItems={cart.total_items}/>
+                <Switch>
+                    <Route exact path="/">
+                        <Products products={products} onAddToCart={handleAddToCart}/>
+                    </Route>
+                    <Route exact path="/cart">
+                        <Cart cart={cart} />
+                    </Route>
+                </Switch>
+            </>
+        </Router>
     )
 }
 
