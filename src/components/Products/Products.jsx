a8ca97112aada5bf46ddb42df5e450e711e415ba
guilhermicas <sotrabalhosgui@hotmail.com>
2021-06-07 20:24:32 +0100

    fetch de produtos da API CommerceJs

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,14 +5,7 @@ import Product from "./Product/Product"
 
 import useStyles from './styles'
 
-const products = [
-    {id:1, name:"laptop", description:"A good laptop", price:"$10", image:"https://pngimg.com/uploads/laptop/laptop_PNG5928.png"},
-    {id:2, name:"Iphone", description:"A good phone", price:"$5", image:"https://phonesdata.com/files/models/Nokia-5800-XpressMusic-736.jpg"},
-    {id:3, name:"Headphones", description:"High quality headphones", price:"$10", image:"https://i5.walmartimages.com/asr/c2a3e351-6e63-431f-ac63-7413ebe48d2d_1.79142d0acd3296ec714280ba991db136.jpeg"},
-]
-
-
-const Products = () => {
+const Products = ({products}) => {
     const classes = useStyles()
 
     return (
